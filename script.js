aa703240006de705b5079276391517816d5d531a
eh377 <85674619+eh377@users.noreply.github.com>
2021-10-27 15:11:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ class App {
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
 
-        window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
+        this.input = document.createElement('input');
+        this.input.type = 'text';
+        document.body.appendChild(this.input);
 
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+        this.text = new Text(this.stageWidth, this.stageHeight, this.ctx, this.input); 
+        this.resize();
         requestAnimationFrame(this.animate.bind(this));
     }
 
@@ -16,7 +22,7 @@ class App {
 
         this.canvas.width = this.stageWidth;
         this.canvas.height = this.stageHeight;
-        this.text = new Text(this.stageWidth, this.stageHeight, this.ctx); 
+        this.text.reset(this.stageWidth, this.stageHeight);
     }
 
     animate() {
@@ -26,24 +32,35 @@ class App {
 }
 
 class Text {
-    constructor(stageWidth, stageHeight, ctx) {
-        this.pointNumber = 20000;
-        this.point = [];
-        for (let i = 0; i < this.pointNumber; i ++) this.point.push(new Point(stageWidth, stageHeight));
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        ctx.font = stageHeight / 2 + 'px 궁서체';
-        ctx.fillStyle = 'blue'
-        ctx.fillText('갓🦍지', stageWidth / 2, stageHeight / 2);
-        this.imageData = ctx.getImageData(0, 0, stageWidth, stageHeight).data;
-        ctx.clearRect(0, 0, stageWidth, stageHeight);
+    constructor(stageWidth, stageHeight, ctx, input) {
+        this.text = "💩"
+
+        this.pointNumber = 30000;
+
+        input.value = '';
 
         this.ctx = ctx;
         this.pointRadius = 3;
         this.clickRadius = 100;
-        this.stageWidth = stageWidth;
+        this.input = input;
 
         document.addEventListener('click', this.click.bind(this));
+        document.addEventListener('keyup', this.enter.bind(this));
+    }
+
+    reset(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+        this.point = [];
+        for (let i = 0; i < this.pointNumber; i ++) this.point.push(new Point(stageWidth, stageHeight));
+        
+        this.ctx.textAlign = 'center';
+        this.ctx.textBaseline = 'middle';
+        this.ctx.font = this.stageHeight / 2 + 'px 궁서체';
+        this.ctx.fillStyle = 'blue'
+        this.ctx.fillText(this.text, this.stageWidth / 2, this.stageHeight / 2);
+        this.imageData = this.ctx.getImageData(0, 0, this.stageWidth, this.stageHeight).data;
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
     }
 
     draw() {
@@ -68,9 +85,23 @@ class Text {
         }
     }
 
+    enter(e) {
+        if (e.keyCode == 13) {
+            this.text = this.input.value;
+            for (let i = 0; i < this.point.length; i ++) {
+                this.point[i].color = 'black';
+            }
+            this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+            this.ctx.fillStyle = 'blue'
+            this.ctx.fillText(this.text, this.stageWidth / 2, this.stageHeight / 2);
+            this.imageData = this.ctx.getImageData(0, 0, this.stageWidth, this.stageHeight).data;
+            this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+            this.input.value = '';
+        }
+    }
+
     isColideText(i) {
         this.index = this.point[i].x + this.point[i].y * this.stageWidth;
-        console.log(this.index)
         if (!(this.imageData[this.index * 4] == 0 && this.imageData[this.index * 4 + 1] == 0 && this.imageData[this.index * 4 + 2] == 0)) return 1;
         return 0
     }
@@ -78,8 +109,8 @@ class Text {
 
 class Point {
     constructor(stageWidth, stageHeight) {
-        this.x = Math.floor(Math.random() * (stageWidth + 1));
-        this.y = Math.floor(Math.random() * (stageHeight + 1));
+        this.x = Math.floor(Math.random() * (stageWidth));
+        this.y = Math.floor(Math.random() * (stageHeight));
         this.color = 'black';
     }
 }
